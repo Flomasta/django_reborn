b110927951dca302e9dc32a1507c6ab6bd720c6d
flomasta <zatevi@mail.ru>
2022-01-12 16:18:05 +0300

    django reborn

---
+++ flomaland/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def first_page(request):
+    a = '<h1>Hello world</h1>'
+    txt = '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    return render(request, './index.html',{'txt': txt})
